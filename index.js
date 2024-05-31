4fbf775f536fca53003f566b8c01311089dbf4c3
Chris <167183247+demodrome@users.noreply.github.com>
2024-05-31 15:06:57 +0100

    feat: Add the Book object constructor

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Create a book object
+ * @param {Object} config The configuration object for the constructor
+ * @param {String} config.title The title of the book
+ * @param {String} config.author The author of the book
+ * @param {Number} config.pageCount The number of pages the book has
+ * @param {Boolean} config.isRead Whether or not the book has been read
+ */
+function Book(config = { title: '', author: '', pageCount: 0, isRead: false }) {
+  this.title = config.title;
+  this.author = config.author;
+  this.pageCount = config.pageCount;
+  this.isRead = config.isRead;
+  this.info = function() {
+    return `Title: ${this.title}\nAuthor: ${this.author}\nPages: ${this.pageCount}\nHas been read: ${this.isRead ? 'Yes' : 'No'}`;
+  }
+}
